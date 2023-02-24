47a15a8a82fc8f0a8fa398c97233660b07d31e2a
wingmatt <matt@wingwebhelp.com>
2023-02-23 18:40:28 -0800

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+const Footer = () => {
+  return (
+    <footer>
+        <img alt="Little Lemon Chicago" src="" />
+        <nav>
+          <section>
+            <h2>Doormat Navigation</h2>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+              <li>
+                <a href="/reservations">Reservations</a>
+              </li>
+              <li>
+                <a href="/order">Order Online</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2>Doormat Navigation</h2>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2>Doormat Navigation</h2>
+            <ul>
+            <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+            </ul>
+          </section>
+        </nav>
+      </footer>
+  )
+}
+
+export default Footer
