911ca8126350fee325944f701351caf0b5f9139e
wingmatt <matt@wingwebhelp.com>
2023-02-23 18:40:35 -0800

    Create Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,34 @@
+const Main = () => {
+  return (
+    <main>
+      <section>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Blah blah blah</p>
+        <img alt="Little Lemon Chicago" src="" />
+      </section>
+      <section>
+        <h2>Specials</h2>
+      </section>
+      <section>
+        <ul>
+          <li>Rating card</li>
+          <li>Rating card</li>
+          <li>Rating card</li>
+          <li>Rating card</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>Blah blah blah</p>
+        <aside>
+          <img alt="Little Lemon Chicago" src="" />
+          <img alt="Little Lemon Chicago" src="" />
+        </aside>
+      </section>
+    </main>
+  )
+}
+
+export default Main
