f9e318dd671f1a8dbe4d00f29a0f8550840a3ed4
wingmatt <matt@wingwebhelp.com>
2023-02-23 18:40:05 -0800

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+const Header = () => {
+  return (
+    <header>
+        <nav>
+          <a href="/">
+            <img alt="Little Lemon" src="" />
+          </a>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/order">Order Online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+  )
+}
+
+export default Header
