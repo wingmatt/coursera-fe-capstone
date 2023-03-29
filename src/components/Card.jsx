baf57729589f2f6843ca65b9cccaad5ddc5d6633
wingmatt <matt@wingwebhelp.com>
2023-03-28 22:36:02 -0700

    Adjust style to match mockup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = (props) => {
   return (
     <article className="card">
-      <img alt={props.title} src={props.imgUrl} width="300" height="200" />
+      <img alt={props.title} src={props.imgUrl} width="270" height="180" />
       <h3>{props.title}</h3>
       <span className="price">${props.price}</span>
       <p>{props.description}</p>
