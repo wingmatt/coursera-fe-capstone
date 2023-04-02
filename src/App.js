a995e060ce462df79e4e0826450e0135e184cf7e
wingmatt <matt@wingwebhelp.com>
2023-04-01 20:58:04 -0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
       <Main/>
       <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Home.jsx
@@ -0,0 +1,57 @@
+import Card from "../components/Card";
+import Testimonial from "../components/Testimonial";
+
+const Home = () => {
+  return (
+    <>
+      <section className="hero bg-primary">
+          <div className="grid-content">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>Alias ea quisquam provident voluptates. Quam eligendi blanditiis itaque ullam aut corporis. Labore aut consequatur.</p>
+            <a href="/reserve-table" className="button cta">Reserve a Table</a>
+            <img  className="imagery" alt="Little Lemon Chicago" src="http://via.placeholder.com/300x200" />
+          </div>
+        </section>
+        <section className="highlights">
+          <div className="grid-content">
+            <h2>Specials</h2>
+            <a href="/order-online" className="button cta">
+              Order Online
+            </a>
+            <ul>
+              <li>
+                <Card title="Greek Salad" description="Description" price="X.YY" imgUrl="http://via.placeholder.com/350x200" />
+              </li>
+              <li>
+                <Card title="Greek Salad" description="Description" price="X.YY" imgUrl="http://via.placeholder.com/350x200" />
+              </li>
+              <li>
+                <Card title="Greek Salad" description="Description" price="X.YY" imgUrl="http://via.placeholder.com/350x200" />
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="testimonials">
+          <ul>
+            <li><Testimonial name="Ralph" imgUrl="http://via.placeholder.com/75x75" rating="5/5" quotation="Great place, great food... great FUN!"/></li>
+            <li><Testimonial name="Ralph" imgUrl="http://via.placeholder.com/75x75" rating="5/5" quotation="Great place, great food... great FUN!"/></li>
+            <li><Testimonial name="Ralph" imgUrl="http://via.placeholder.com/75x75" rating="5/5" quotation="Great place, great food... great FUN!"/></li>
+          </ul>
+        </section>
+        <section className="about">
+          <div className="grid-content">
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p>Alias ea quisquam provident voluptates. Quam eligendi blanditiis itaque ullam aut corporis. Labore aut consequatur. Alias ea quisquam provident voluptates. Quam eligendi blanditiis itaque ullam aut corporis. Labore aut consequatur.</p>
+            <aside className="imagery">
+              <img alt="Little Lemon Chicago" src="http://via.placeholder.com/200x300" />
+              <img alt="Little Lemon Chicago" src="http://via.placeholder.com/200x300" />
+            </aside>
+          </div>
+        </section>
+      </>
+  )
+}
+
+export default Home
