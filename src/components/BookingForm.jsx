d1f9ba8de7f7bea4490b9d64c95012534bd2bb03
wingmatt <matt@wingwebhelp.com>
2023-04-05 08:08:50 -0700

    Change: for attr to htmlFor

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,9 +1,9 @@
-const BookingForm = () => {
+const BookinghtmlForm = () => {
   return(
     <form className="booking">
-      <label for="res-date">Choose date</label>
+      <label htmlFor="res-date">Choose date</label>
       <input type="date" id="res-date" />
-      <label for="res-time">Choose time</label>
+      <label htmlFor="res-time">Choose time</label>
       <select id="res-time ">
           <option>17:00</option>
           <option>18:00</option>
@@ -12,9 +12,9 @@ const BookingForm = () => {
           <option>21:00</option>
           <option>22:00</option>
       </select>
-      <label for="guests">Number of guests</label>
+      <label htmlFor="guests">Number of guests</label>
       <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <label for="occasion">Occasion</label>
+      <label htmlFor="occasion">Occasion</label>
       <select id="occasion">
           <option>Birthday</option>
           <option>Anniversary</option>
