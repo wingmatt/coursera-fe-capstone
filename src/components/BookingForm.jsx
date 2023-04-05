4287c5311313936d17b0857ee94ac5a30c6ccb15
wingmatt <matt@wingwebhelp.com>
2023-04-05 08:07:41 -0700

    Add: BookingForm with initial structure

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,25 @@
+const BookingForm = () => {
+  return(
+    <form className="booking">
+      <label for="res-date">Choose date</label>
+      <input type="date" id="res-date" />
+      <label for="res-time">Choose time</label>
+      <select id="res-time ">
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+      </select>
+      <label for="guests">Number of guests</label>
+      <input type="number" placeholder="1" min="1" max="10" id="guests" />
+      <label for="occasion">Occasion</label>
+      <select id="occasion">
+          <option>Birthday</option>
+          <option>Anniversary</option>
+      </select>
+      <input type="submit" value="Make Your reservation"></input>
+    </form>
+  )
+}
