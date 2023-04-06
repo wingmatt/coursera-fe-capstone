1584ce007ddbce81be1397d792b0f235f93483f3
wingmatt <matt@wingwebhelp.com>
2023-04-05 17:53:25 -0700

    Add: updateTimes function. useEffect to set availableTImes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "../routes/Home";
 import ReserveTable from "../routes/ReserveTable";
 
+
+
 const Main = () => {
-  const [availableTimes, setAvailableTimes] = useState([
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
+  const [availableTimes, setAvailableTimes] = useState([]);
+  useEffect(() => updateTimes(), [])
+  const updateTimes = (selectedDate) => {
+    let updatedTimes;
+    switch(selectedDate) {
+      default:
+        updatedTimes =
+        [
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+        ];
+        break;
+    }
+    setAvailableTimes(updatedTimes)
+  }
   return (
     <main>
       <Routes>
