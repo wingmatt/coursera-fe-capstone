606ef87f996f752c6df6b1a88abcc4059775c955
wingmatt <matt@wingwebhelp.com>
2023-04-07 08:39:10 -0700

    Add test for form submission

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,9 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from './BookingForm';
-import Main from "./Main";
+import {jest} from '@jest/globals';
 
 test('Renders the BookingForm heading', () => {
     render(<BookingForm />);
     const headingElement = screen.getByText("Reserve a Table");
     expect(headingElement).toBeInTheDocument();
+})
+
+test('Form can be submitted', () => {
+    const handleSubmit = jest.fn();
+    render(<BookingForm onSubmit={handleSubmit} />);
+    fireEvent.submit(screen.getByRole("form"));
+    expect(handleSubmit).toHaveBeenCalled()
 })
