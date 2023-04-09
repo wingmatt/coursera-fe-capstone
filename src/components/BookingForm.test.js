51ba7a4598a1ebe01f46a81e79c27beaeb793ec9
wingmatt <matt@wingwebhelp.com>
2023-04-09 11:08:03 -0700

    Fix: useNavigate test error

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,16 +1,17 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from './BookingForm';
 import {jest} from '@jest/globals';
+import { BrowserRouter } from "react-router-dom";
 
 test('Renders the BookingForm heading', () => {
-    render(<BookingForm />);
+    render(<BrowserRouter><BookingForm /></BrowserRouter>);
     const headingElement = screen.getByText("Reserve a Table");
     expect(headingElement).toBeInTheDocument();
 })
 
 test('Form can be submitted', () => {
     const handleSubmit = jest.fn();
-    render(<BookingForm onSubmit={handleSubmit} />);
+    render(<BrowserRouter><BookingForm onSubmit={handleSubmit} /></BrowserRouter>);
     fireEvent.submit(screen.getByRole("form"));
     expect(handleSubmit).toHaveBeenCalled()
 })
