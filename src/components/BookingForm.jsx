51f144840d5b66acc5a37362b2ab431acdfefa55
wingmatt <matt@wingwebhelp.com>
2023-04-11 08:23:34 -0700

    Change: layout, styling for form

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -33,23 +33,26 @@ const BookingForm = ({availableTimes, dispatch, onSubmit}) => {
   return (
     <form className="booking" onSubmit={(event) => onSubmit(event, form.data, navigate)} aria-label="Make a reservation" id="booking-form">
       <h2>Reserve a Table</h2>
-      <label htmlFor="resDate">Choose date</label>
-      <input
-        type="date"
-        id="resDate"
-        required
-        value={form.data.resDate}
-        onChange={(event) => handleChange(event)}
-        className={form.data.errors.resDate && "invalid"}
-      />
-      {form.data.errors.resDate && <span className="error">Please enter your desired reservation date.</span>}
-      <label htmlFor="resTime">Choose time</label>
-      <h3>Available Times:</h3>
+      <div className="input-container">
+        <label htmlFor="resDate">What day?</label>
+        <input
+          type="date"
+          id="resDate"
+          required
+          value={form.data.resDate}
+          onChange={(event) => handleChange(event)}
+          className={form.data.errors.resDate && "invalid"}
+        />
+        {form.data.errors.resDate && <span className="error">Please enter your desired reservation date.</span>}
+      </div><div className="input-container">
+      <aside><h3>Available Times:</h3>
       <ul className="available-times">
         {Array.isArray(availableTimes) ? availableTimes.map((time) => (
             <li key={time}>{time}</li>
           )) : ""}
       </ul>
+      </aside>
+      <label htmlFor="resTime">What time?</label>
       <select
         id="resTime"
         value={form.data.resTime}
@@ -57,12 +60,13 @@ const BookingForm = ({availableTimes, dispatch, onSubmit}) => {
         onChange={(event) => handleChange(event)}
         className={form.data.errors.resTime && "invalid"}
       >
-        <option disabled hidden value="">Select a Time</option>
+        <option disabled hidden value=""></option>
         {Array.isArray(availableTimes) ? availableTimes.map((time) => (
           <option key={time}>{time}</option>
         )) : ""}
       </select>
       {form.data.errors.resTime && <span className="error">Please enter your desired reservation time.</span>}
+      </div><div className="input-container">
       <label htmlFor="guests">Number of guests</label>
       <input
         type="number"
@@ -76,16 +80,18 @@ const BookingForm = ({availableTimes, dispatch, onSubmit}) => {
         className={form.data.errors.guests && "invalid"}
       />
       {form.data.errors.guests && <span className="error">Please enter the number of guests.</span>}
+      </div><div className="input-container">
       <label htmlFor="occasion">Occasion</label>
       <select
         id="occasion"
         value={form.data.occasion}
         onChange={(event) => handleChange(event)}
       >
-        <option disabled hidden value="">Select an Occasion</option>
+        <option disabled hidden value=""></option>
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
+      </div>
       <input type="submit" value="Make Your reservation" className="button" disabled={!form.data.submittable}></input>
     </form>
   );
