bf6eed275e4116c1a48c80de1ca0fd86b7f250a3
wingmatt <matt@wingwebhelp.com>
2023-04-12 17:41:43 -0700

    Change: Make all links go somewhere real

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,19 +12,19 @@ const Header = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/">About</Link>
             </li>
             <li>
-              <Link to="/menu">Menu</Link>
+              <Link to="/">Menu</Link>
             </li>
             <li>
               <Link to="/reserve-table">Reservations</Link>
             </li>
             <li>
-              <Link to="/order">Order Online</Link>
+              <Link to="/">Order Online</Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/">Login</Link>
             </li>
           </ul>
         </nav>
